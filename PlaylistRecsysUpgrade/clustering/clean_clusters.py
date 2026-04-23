f0c95d82f34088b1fbb3a8ebaa9a1c29ca6fd318
siddmohanty111 <siddhant_99@hotmail.com>
2026-04-23 14:24:37 -0700

    removed unused cluster cleaning file

--- PlaylistRecsysUpgrade/clustering/clean_clusters.py
+++
@@ -1,7 +0,0 @@
-"""
-
-A set of cleaning functions to be applied to the clusters obtained from the clustering step. Instead of dropping clusters where the modal playlist title
-appears in <= 2% of clusters, we perform a locality sensitive hashing (LSH) step inside each cluster to identify 100 subclusters of playlists with similar titles, and drop clusters where the largest subcluster contains <= 2% of the playlists in the cluster.
-
-"""
-
